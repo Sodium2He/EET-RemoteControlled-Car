dc0d879db5d4c47c0981724af4c048be30e1bb03
M-10777 <98745617+M-10777@users.noreply.github.com>
2024-04-14 12:56:22 +0800

    初步使用websocket连接网页前后端（烦。。。）

--- Wireless Connection/setup-server.py
+++ Wireless Connection/setup-server.py
@@ -1,29 +1,38 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
   # modules for web server
+from flask_socketio import SocketIO, send, emit
+  # for WebSocket
 import socket
-  # modules for UDP connection
+  # modules for UDP connection (ESP8266)
 
 # UDP settings
 # change IP & PORT to yours!
 espIP = '192.168.1.10'
 espPORT = 16384
-sockAddr = (espIP, espPORT)
+espAddr = (espIP, espPORT)
 espSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   # socket.AF_INET -> use IPv4; socket.SOCK_DGRAM -> UDP
 espSocket.setblocking(0)
 
-# flask
+# web server config
 app = Flask(__name__)
+webSocketio = SocketIO(app)
 @app.route("/")
-def index():
+def index(): # web
   return render_template('index.html')
 
+# handling the WebSocket connection
+@webSocketio.on('message')
+def handleMessage(message):
+  sendUDPpackage(message)
+
+#run server
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=25564, debug=True)
 
 # udp
 def sendUDPpackage(data):
-  espSocket.sendto(data, sockAddr)
+  espSocket.sendto(data, espAddr)
 
 def recvUDPpackage():
   try:
