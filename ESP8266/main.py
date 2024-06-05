54b9568264a6e77c317a860a7246a2fdeeac3d1a
M-10777 <coreius@outlook.com>
2024-06-05 16:21:11 +0800

    加快循环速度

--- ESP8266/main.py
+++ ESP8266/main.py
@@ -5,9 +5,13 @@ import pwm
 
 from machine import PWM,Pin
 from time import sleep
-servo=PWM(Pin(12),freq=50,duty=0)#�?向控�?
-pwm1=PWM(Pin(4),freq=50,duty=0)#两路pwm波控制前进方�?
+
+# controls turning
+servo=PWM(Pin(12),freq=50,duty=0)
+# controls speed + linear motion
+pwm1=PWM(Pin(4),freq=50,duty=0)
 pwm2=PWM(Pin(5),freq=50,duty=0)
+
 def get_duty(direction):
    duty=(10/18)*direction
    return int(duty)
@@ -17,7 +21,7 @@ wlanSSID = '8b2bnsd9g'
 wlanPWD  = 'hje9cb3mi'
 
 # UDP settings, change PORT to yours!
-espPORT = 16384
+espPORT = 65535
 espSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Wi-Fi connection
@@ -38,7 +42,7 @@ while True:
     print('ESP8266> from', sockAddr, 'Received:', data)
     # sock.sendto()
     if data:
-      message = data.decode()  # 将bytes解码为字符串
+      message = data.decode()
       if ':' in message:
          parts = message.split(':')
          key = int(ord(parts[1].strip('"')))
@@ -47,15 +51,15 @@ while True:
             if key == 115:
               pwm2.duty(get_duty(90))
               pwm1.duty(0)
-              sleep(1)
+              sleep(10)
             elif key == 119:
               pwm1.duty(get_duty(60))
               pwm2.duty(0)
-              sleep(1)
+              sleep(10)
          elif status == 0:
             pwm1.duty(1023)
             pwm2.duty(1023)
-            sleep(1) 
+            sleep(10) 
     break        
 
 
